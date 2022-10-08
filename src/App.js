2c69b43a06257d977ce156925799351798126fea
jesseClegg <89627047+jesseClegg@users.noreply.github.com>
2022-10-08 18:03:24 -0400

    loads s3 image

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import ContainerFluidExample from "./someComponent";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-
-
+      <img
+          src="https://s3forninad.s3.amazonaws.com/jeffreyDahmer.jpg"
+          alt="new"
+      />
   );
 }
 
