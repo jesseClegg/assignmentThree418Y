c84d8658cfce86ce6ce477c2a58f0fa9893a7534
jesseClegg <89627047+jesseClegg@users.noreply.github.com>
2022-10-08 21:14:35 -0400

    changed image size, added title and paragraph

--- src/App.js
+++ src/App.js
@@ -4,10 +4,25 @@ import './App.css';
 
 function App() {
   return (
+      <>
+
+      <container><h1>
+          Jefferson Dahmer the Third
+      </h1>
+          <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+          </p>
+
       <img
+          width={300}
+          height={300}
           src="https://s3forninad.s3.amazonaws.com/jeffreyDahmer.jpg"
           alt="new"
       />
+
+
+      </container>
+      </>
   );
 }
 
