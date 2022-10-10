6860d1ab0365b0a8ee896917511e765449c4a86a
jesseClegg <89627047+jesseClegg@users.noreply.github.com>
2022-10-09 20:41:51 -0400

    changed image size, added title and paragraph

--- src/App.js
+++ src/App.js
@@ -1,27 +1,50 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import './App.css';
+import {Container} from "react-bootstrap";
 
 
 function App() {
   return (
       <>
+          <Container style={ {backgroundColor:"f1f1f1"} }>
+          <Row>
+              <Col>
+                  <img width={20} height={20} src="https://getbootstrap.com/docs/5.2/assets/img/favicons/favicon-32x32.png" />
+              </Col>
+              <Col>
+                  Media Library
+              </Col>
+          </Row>
+          </Container>
+              <Container>
+            <Row>
 
-      <container><h1>
-          Jesse LastName
-      </h1>
-          <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-          </p>
+                <Col>
+                    <img
+                        width={300}
+                        height={300}
+                        src="https://s3forninad.s3.amazonaws.com/jeffreyDahmer.jpg"
+                        alt="new"
+                    />
+                </Col>
+                <Col>
+                    <h2>
+                        Jesse LastName
+                    </h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. E
+                        xcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+                    </p>
+                </Col>
 
-      <img
-          width={300}
-          height={300}
-          src="https://s3forninad.s3.amazonaws.com/jeffreyDahmer.jpg"
-          alt="new"
-      />
-
-
-      </container>
+            </Row>
+          </Container>
       </>
   );
 }
