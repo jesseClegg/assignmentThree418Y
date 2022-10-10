18466f7ff70c0cf861c104a69d0e902411805d42
jesseClegg <89627047+jesseClegg@users.noreply.github.com>
2022-10-09 23:04:24 -0400

    slight touch up, included express

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
                             Jesse LastName
                         </h2>
                     </Row>
-                   <Row>
+                   <Row style={ { marginTop: 15} }>
                        <p>
                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
